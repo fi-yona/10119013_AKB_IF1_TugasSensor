6c8e8cc2791c78fb8d1211c1805b75aac4cc98d2
fi-yona <fionaavila15@gmail.com>
2022-06-28 15:06:59 +0700

    Add Splash Screen

---
+++ app/src/main/java/com/example/a10119013_akb_if1_tugassensor/SplashScreenActivity.java
@@ -0,0 +1,19 @@
+package com.example.a10119013_akb_if1_tugassensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private Button button_mulai;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+    }
+}
