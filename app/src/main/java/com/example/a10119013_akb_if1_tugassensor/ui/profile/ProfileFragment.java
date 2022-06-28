b37a609815d7ca0a9a6b5da8847b2c66ddc1e831
fi-yona <fionaavila15@gmail.com>
2022-06-28 17:43:28 +0700

    Update Menu Profile

--- app/src/main/java/com/example/a10119013_akb_if1_tugassensor/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/a10119013_akb_if1_tugassensor/ui/profile/ProfileFragment.java
@@ -1,4 +1,4 @@
-package com.example.a10119013_akb_if1_tugassensor.ui.home;
+package com.example.a10119013_akb_if1_tugassensor.ui.profile;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.a10119013_akb_if1_tugassensor.databinding.FragmentHomeBinding;
+import com.example.a10119013_akb_if1_tugassensor.databinding.FragmentProfileBinding;
 
-public class HomeFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private FragmentProfileBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        ProfileViewModel profileViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/example/a10119013_akb_if1_tugassensor/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/example/a10119013_akb_if1_tugassensor/ui/profile/ProfileViewModel.java
@@ -1,14 +1,14 @@
-package com.example.a10119013_akb_if1_tugassensor.ui.home;
+package com.example.a10119013_akb_if1_tugassensor.ui.profile;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class ProfileViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public ProfileViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
     }
