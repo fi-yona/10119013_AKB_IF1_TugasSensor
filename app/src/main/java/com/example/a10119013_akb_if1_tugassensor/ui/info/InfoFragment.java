a8f104c3545c0910c04e886062d0562b907c7925
fi-yona <fionaavila15@gmail.com>
2022-06-28 15:25:46 +0700

    Add Menu Tentang Aplikasi at Menu Drawer

---
+++ app/src/main/java/com/example/a10119013_akb_if1_tugassensor/ui/info/InfoFragment.java
@@ -0,0 +1,42 @@
+package com.example.a10119013_akb_if1_tugassensor.ui.info;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.a10119013_akb_if1_tugassensor.R;
+import com.example.a10119013_akb_if1_tugassensor.databinding.FragmentInfoBinding;
+
+public class InfoFragment extends Fragment {
+
+    private FragmentInfoBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        InfoViewModel infoViewModel =
+                new ViewModelProvider(this).get(InfoViewModel.class);
+
+        binding = FragmentInfoBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textInfo;
+        infoViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
