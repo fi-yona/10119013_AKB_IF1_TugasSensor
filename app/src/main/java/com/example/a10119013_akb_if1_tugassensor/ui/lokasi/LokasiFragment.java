df7e75c793b0d44252034e9bdb7af81c2d6c756c
fi-yona <fionaavila15@gmail.com>
2022-06-29 16:22:23 +0700

    Add map at Lokasi Saat Ini

--- app/src/main/java/com/example/a10119013_akb_if1_tugassensor/ui/lokasi/LokasiFragment.java
+++ app/src/main/java/com/example/a10119013_akb_if1_tugassensor/ui/lokasi/LokasiFragment.java
@@ -1,37 +1,51 @@
 package com.example.a10119013_akb_if1_tugassensor.ui.lokasi;
 
+import androidx.fragment.app.FragmentActivity;
+
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import com.example.a10119013_akb_if1_tugassensor.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.a10119013_akb_if1_tugassensor.databinding.ActivityLokasiBinding;
 
-import com.example.a10119013_akb_if1_tugassensor.databinding.FragmentLokasiBinding;
+public class LokasiActivity extends FragmentActivity implements OnMapReadyCallback {
 
-public class LokasiFragment extends Fragment {
-
-    private FragmentLokasiBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        LokasiViewModel lokasiViewModel =
-                new ViewModelProvider(this).get(LokasiViewModel.class);
-
-        binding = FragmentLokasiBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textSlideshow;
-        lokasiViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
-    }
+    private GoogleMap mMap;
+    private ActivityLokasiBinding binding;
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityLokasiBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng kampus = new LatLng(-6.886139977661389, 107.61534585421778);
+        mMap.addMarker(new MarkerOptions().position(kampus).title("Marker in UNIKOM"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(kampus));
     }
 }
